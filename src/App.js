a1a82b89c2880a87ae7f89ed64ba78b4858f17df
Roman Yarema <romanyarema33@gmail.com>
2025-03-07 23:26:32 +0200

    okok

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import Home from './components/Home';
-import Music from './components/Music';
-import Letters from './components/Letters'
-import Images from './components/Images';
+import Home from './Home';
+import Music from './Music';
+import Letters from './Letters'
+import Images from './Images';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <div className='App'>
       <Routes>
         <Route index element={<Home />} />
         <Route path='/music' element={<Music />} />
         <Route path='/letters' element={<Letters />} />
         <Route path='/images' element={<Images />} />
       </Routes>
+    </div>
   );
 }
 
